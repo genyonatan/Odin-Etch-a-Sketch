6fe43ffb52f7e6f4158bde6f550c64076028ef94
Yonatan Genene <yonatangenene30@gmail.com>
2025-11-19 09:51:38 +0300

    added gird manipulation function via button

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-const mainContainer = document.querySelector(".main-container");
+const mainContainer = document.querySelector(".main-container"); // select main container div
+const cellBtn = document.querySelector("#cellsModifyBtn"); // select button to modify cells
 
-const totalCells = 16;
+createGrid(4); // defualt grid of 4 cells per side
 
-for (let i = 0; i < totalCells; i++) {
-  const cell = document.createElement("div");
-  cell.classList.add("cell");
-  mainContainer.appendChild(cell);
+cellBtn.addEventListener("click", () => {
+  let userInput = prompt("Enter number of cells per side (max 100):");
+
+  let cellsPerSide = parseInt(userInput);
+
+  if (isNaN(cellsPerSide) || cellsPerSide < 1 || cellsPerSide > 64) {
+    alert("Please enter a valid number between 1 and 100.");
+    return;
+  } else {
+    createGrid(cellsPerSide);
+  }
+});
+
+function createGrid(cellsPerSide) {
+  // clear existing cells and create a new grid
+  mainContainer.innerHTML = "";
+  mainContainer.style.gridTemplateColumns = `repeat(${cellsPerSide}, 1fr)`;
+
+  // calculate total new cells and crete them
+  const totalNewCells = cellsPerSide * cellsPerSide;
+  for (let i = 0; i < totalNewCells; i++) {
+    const newCell = document.createElement("div");
+    newCell.classList.add("cell");
+    mainContainer.appendChild(newCell);
+  }
 }
